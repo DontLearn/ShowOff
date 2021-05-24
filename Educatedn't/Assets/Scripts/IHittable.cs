2df595539e37dd63342431402075e262608271e2
Jos Barkman <jos.barkman@hotmail.com>
2021-05-24 12:01:11 +0200

    BattleSystem
    Created attacks and hit points

---
+++ Educatedn't/Assets/Scripts/IHittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Combat
+{
+    interface IHittable
+    {
+        public void Hit( int damage );
+        public void Hit( int damage, Vector3 force );
+        public void Die();
+        public bool IsDead();
+    }
+}
