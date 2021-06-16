7df7db8abe85bcb78f1107f741c5c03b9905826f
KristynaPavlatova <krpavlatova@gmail.com>
2021-06-16 11:38:55 +0200

    Ingredient Pickups scene 1 fix

--- Educatednt/Assets/Kristyna/Scripts/PortalTest.cs
+++ Educatednt/Assets/Kristyna/Scripts/PortalTest.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(6);//Kitchen scene
+        }
     }
 }
