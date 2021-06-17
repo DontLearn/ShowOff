611594eed761931ef5638bae5b97a106d335f091
EvgeniiDembovskii <eugenedembowski@mail.ru>
2021-06-17 15:05:10 +0300

    Some random change tutorial script

--- Educatednt/Assets/TutorialManager.cs
+++ Educatednt/Assets/TutorialManager.cs
@@ -12,7 +12,7 @@ public class TutorialManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DefineStage();
     }
 
    public void DefineStage()
